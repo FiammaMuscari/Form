3280f8273bc2a6575c4aa47436c0edfc0e5f9805
fiammamuscari <fiammamuscari@gmail.com>
2024-03-03 23:37:54 -0300

    fix: destructuring for crudestate management && deleting zod

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -1,45 +1,94 @@
 import React, { useState } from "react";
-import { validateSignup } from "../utils/validation";
-import { z } from "zod";
 
 const SignupForm: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState<string | null>(null);
-  const [passwordError, setPasswordError] = useState<string | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    emailError: "",
+    passwordErrors: {
+      lengthError: false,
+      numberError: false,
+      specialCharacterError: false,
+    },
+    successMessage: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+      passwordErrors:
+        name === "password" ? validatePassword(value) : formData.passwordErrors,
+    });
+  };
+
+  const validatePassword = (password: string) => {
+    const passwordErrors = {
+      lengthError: password.length >= 8 ? true : false,
+      numberError: /\d/.test(password),
+      specialCharacterError: /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(
+        password
+      ),
+    };
+    return passwordErrors;
+  };
 
   const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Data:", { email, password });
-    try {
-      validateSignup({ email, password });
-      setEmailError(null);
-      setPasswordError(null);
-      setSuccessMessage("Signup successful! Welcome aboard!");
-    } catch (err) {
-      if (err instanceof z.ZodError) {
-        err.errors.forEach((error) => {
-          if (error.path[0] === "email") {
-            setEmailError(error.message);
-          } else if (error.path[0] === "password") {
-            setPasswordError(error.message);
-          }
-        });
-      } else {
-        setEmailError("An unexpected error occurred");
-        setPasswordError("An unexpected error occurred");
-      }
-      setSuccessMessage(null);
+    const { email, password } = formData;
+
+    if (!validateEmail(email)) {
+      setFormData({
+        ...formData,
+        emailError: "Formato del mail inválido",
+        passwordErrors: {
+          lengthError: false,
+          numberError: false,
+          specialCharacterError: false,
+        },
+        successMessage: "",
+      });
+      return;
     }
+
+    const errors = validatePassword(password);
+    if (
+      !errors.lengthError ||
+      !errors.numberError ||
+      !errors.specialCharacterError
+    ) {
+      setFormData({
+        ...formData,
+        emailError: "",
+        passwordErrors: errors,
+        successMessage: "",
+      });
+      return;
+    }
+
+    setFormData({
+      ...formData,
+      emailError: "",
+      passwordErrors: errors,
+      successMessage: "Sesión iniciada! Bienvenido!",
+    });
+  };
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /\S+@\S+\.\S+/;
+    return emailRegex.test(email);
   };
 
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <form onSubmit={handleSignup} className="w-full max-w-md">
-        <h2 className="text-2xl mb-4 ">Sign Up</h2>
-        {successMessage && (
-          <div className="mb-4 text-green-500">{successMessage}</div>
+    <div className="flex justify-center items-center h-screen">
+      <form
+        onSubmit={handleSignup}
+        className="border-white border-2 border-solid p-4 rounded-md"
+      >
+        <h2 className="text-2xl mb-4">Registro</h2>
+        {formData.successMessage && (
+          <div className="mb-4 text-green-500">{formData.successMessage}</div>
         )}
         <div className="mb-4">
           <label htmlFor="email" className="block text-white mb-1">
@@ -48,12 +97,15 @@ const SignupForm: React.FC = () => {
           <input
             type="email"
             id="email"
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            className="w-full p-1 rounded-lg border-white border-2 border-solid"
+            value={formData.email}
+            onChange={handleInputChange}
             required
           />
-          {emailError && <div className="text-red-500 mb-1">{emailError}</div>}
+          {formData.emailError && (
+            <div className="text-red-500 mb-1">{formData.emailError}</div>
+          )}
         </div>
         <div className="mb-4">
           <label htmlFor="password" className="block text-white mb-1">
@@ -62,21 +114,52 @@ const SignupForm: React.FC = () => {
           <input
             type="password"
             id="password"
-            className="w-full px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            className="w-full p-1 rounded-lg border-white border-2 border-solid"
+            value={formData.password}
+            onChange={handleInputChange}
             required
             autoComplete="current-password"
           />
-          {passwordError && (
-            <div className="text-red-500 mb-1">{passwordError}</div>
-          )}
+          <div className="mb-1">
+            {formData.passwordErrors.lengthError ? (
+              <span className="text-green-500">
+                La contraseña tiene al menos 8 letras
+              </span>
+            ) : (
+              <span className="text-red-500">
+                La contraseña debe tener al menos 8 letras
+              </span>
+            )}
+          </div>
+          <div className="mb-1">
+            {formData.passwordErrors.numberError ? (
+              <span className="text-green-500">
+                La contraseña tiene al menos un número
+              </span>
+            ) : (
+              <span className="text-red-500">
+                La contraseña debe tener al menos un número
+              </span>
+            )}
+          </div>
+          <div className="mb-1">
+            {formData.passwordErrors.specialCharacterError ? (
+              <span className="text-green-500">
+                La contraseña tiene al menos un carácter especial
+              </span>
+            ) : (
+              <span className="text-red-500">
+                La contraseña necesita un carácter especial
+              </span>
+            )}
+          </div>
         </div>
         <button
           type="submit"
           className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
         >
-          clic me
+          Registrarse
         </button>
       </form>
     </div>
